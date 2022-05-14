ae8185f828efb3399b20180eb9d2f2160368480f
afrikalileo <veyseltekin1683@gmail.com>
2022-05-14 18:32:21 +0300

    tabloedit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -9,18 +9,46 @@ UserPhoneN nvarchar(MAX),
 UserAddress nvarchar(MAX),
 UserCity nvarchar(MAX),
 UserVillage nvarchar(MAX),
+
 )
 
 
-create table Fault
+create table Fault2
 (
 ID int Primary Key IDENTITY,
 DateofReceipt date,
 UserDesc nvarchar(MAX),
 FaultTechInfo nvarchar(MAX),
 FaultCategory nvarchar(MAX),
-Payment nvarchar(MAX),
+Payment money,
 DueDate date,
 FaultResp nvarchar(MAX),
 FaultStep nvarchar(MAX),
+ProductCatID int,
+EmployeeID int,
+ProductID int,
 )
+
+create table Employee
+(
+ID int Primary Key IDENTITY,
+EmployeeName nvarchar(MAX),
+EmployeeSurname nvarchar(MAX),
+)
+
+create table Productf
+(
+ID int Primary Key IDENTITY,
+ProductName nvarchar(max),
+)
+
+create table ProductCate
+(
+ID int Primary Key IDENTITY(1,1),
+ProductCatID int,
+CategoryName nvarchar(MAX),
+CategoryID int,
+)
+
+
+
